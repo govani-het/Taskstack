05921d01ae84273fe5d1923cbda3e3a634e29daa
Het <het.inexture@gmail.com>
2026-03-28 16:56:58 +0530

    Implemented V2 improvements based on feedback

--- 12. python/Module_3/binary_search.py
+++ 12. python/Module_3/binary_search.py
@@ -2,12 +2,12 @@ from insertion_sort import insertion_sorts
 
 
 
-def binary_search(list_of_number, number):
+def binary_search(list_of_numbers, number):
     """
     Perform binary search on a sorted list to find a target number.
 
     Args:
-        list_of_number (list[int]): A sorted list of integers where the
+        list_of_numbers (list[int]): A sorted list of integers where the
             search operation will be performed.
         number (int): The target value to search for in the list.
 
@@ -16,21 +16,28 @@ def binary_search(list_of_number, number):
         prints that the element was not found.
     """
     start = 0
-    end = len(list_of_number) - 1
+    end = len(list_of_numbers) - 1
     while start <= end:
         mid = (start + end) // 2
-        if list_of_number[mid] == number:
+        if list_of_numbers[mid] == number:
             print("Element is present at index " + str(mid))
             return
-        elif list_of_number[mid] < number:
+        elif list_of_numbers[mid] < number:
             start = mid + 1
         else:
             end = mid - 1
     print("Element not found")
 
 
-list_of_number = [23,45,67,334,56]
-sorted_list = insertion_sorts(list_of_number)
+list_of_numbers = [23,45,67,334,56]
+sorted_list = insertion_sorts(list_of_numbers)
 print(sorted_list)
-number = int(input("Enter the number: "))
+
+while True:
+    try:
+        number = int(input("Enter the number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
 binary_search(sorted_list, number)
